c1094442445962761f7d8f8f07643a95f4512afd
NEAmico <lishay.matvey20001506@gmail.com>
2023-09-29 17:14:44 +0200

    test autodeploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+          GET Started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
         <div>
