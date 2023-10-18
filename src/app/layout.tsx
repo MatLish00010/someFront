1882d47449558c0987f69601209ae1de5b8e8e97
NEAmico <lishay.matvey20001506@gmail.com>
2023-10-18 12:00:57 +0200

    connect tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import type {Metadata} from 'next';
-import {Inter} from 'next/font/google';
 import React from 'react';
 
 import './globals.css';
-
-const inter = Inter({subsets: ['latin']});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -13,8 +10,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="h-screen	flex">
+      <body className="flex-1 flex">
+        <main className="flex-1">{children}</main>
+      </body>
     </html>
   );
 }
